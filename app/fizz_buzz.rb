5294767ff15897503191a7a07945a1fa431ab3b3
Mikito Yoshiya <yoshiya@wonderpla.net>
2022-09-06 00:53:11 +0900

    step9-2

--- app/fizz_buzz.rb
+++ app/fizz_buzz.rb
@@ -1,7 +1,7 @@
 class FizzBuzz
   def evaluate(val)
+    return "fizzbuzz" if (val % 3 == 0) && (val % 5 == 0)
     return "fizz" if val % 3 == 0
     return "buzz" if val % 5 == 0
-    return "fizzbuzz" if (val % 3 == 0) && (val % 5 == 0)
   end
 end
