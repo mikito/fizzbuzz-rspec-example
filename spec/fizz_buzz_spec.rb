421ff77456d0bf77432aa779fd9904f7681db815
Mikito Yoshiya <yoshiya@wonderpla.net>
2022-09-06 00:22:39 +0900

    step3

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,5 +4,10 @@ describe FizzBuzz do
       fizzbuzz = FizzBuzz.new
       expect(fizzbuzz.evaluate(3)).to eq "fizz"
     end
+
+    it "6を与えたときにfizzを返すこと" do
+      fizzbuzz = FizzBuzz.new
+      expect(fizzbuzz.evaluate(6)).to eq "fizz"
+    end
   end
 end
