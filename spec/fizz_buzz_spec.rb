e41eebfa579835d706eafd11f1222e17b4f2e7f3
Mikito Yoshiya <yoshiya@wonderpla.net>
2022-09-06 00:35:39 +0900

    step8

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -11,6 +11,12 @@ describe FizzBuzz do
       expect(fizzbuzz.evaluate(5)).to eq "buzz"
       expect(fizzbuzz.evaluate(10)).to eq "buzz"
     end
+
+    it "3と5の公倍数を与えたときにfizzbuzzを返すこと" do
+      fizzbuzz = FizzBuzz.new
+      expect(fizzbuzz.evaluate(15)).to eq "fizzbuzz"
+      expect(fizzbuzz.evaluate(45)).to eq "fizzbuzz"
+    end
   end
 end
 
